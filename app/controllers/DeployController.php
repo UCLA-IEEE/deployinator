5935f12bf5f7c3ab6173d6e89cc7210b7151711b
Jeffrey Chan <jeffschan97@gmail.com>
2019-02-25 15:32:04 -0800

    Ok things work again

--- app/controllers/DeployController.php
+++ app/controllers/DeployController.php
@@ -8,6 +8,7 @@ class DeployController extends Controller
     private $repoToDir = array(
         "ieeebruins.com" => "ieeebruins-production",
         "deployinator" => "deployinator",
+        "gb-family-tree" => "gb-family-tree-prod",
     );
 
 
